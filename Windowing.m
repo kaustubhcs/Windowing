bb6e9cd37fd5ea01dbceda94da356850a971c39f
kaustubhcs <kaustubhcs07@gmail.com>
2015-09-11 14:06:01 +0530

    Title Change
    ~KTB

--- Windowing.m
+++ Windowing.m
@@ -1,4 +1,4 @@
-%% Windowing
+%% Windowing Technique
 % Design, visualize, and implement window functions. Compare mainlobe 
 % widths and sidelobe levels of windows as a function of their size and 
 % other parameters.
